07a52c0bd447bf6dad4a0d13b44a430008a6cf7e
kan_chan <shunplay1793@gmail.com>
2026-07-31 12:04:36 +0900

    pattern_c_calculatorメソッドの下準備段階を実装 > 続いて計算ロジックへ （while処理を記述していく）

--- app/services/pattern_c_calculator.rb
+++ app/services/pattern_c_calculator.rb
@@ -17,6 +17,33 @@ class PattertnCCalculator.rb
   private
 
   def pattertn_c_calculator
+    # 趣味と自由時間を全件リストで取得
+    hobby_all = Hooby.all
+    freetime_all = FreeTime.all
+
+    # 曜日のソートを行う（day_of_weekの昇順：同値はないので第二条件なし）
+    sort_days = freetime_all.sort_by { |ft|[ ft.day_of_week ] }
     
+    # 趣味の並び替え（割合の降順：同値ならidの昇順）
+    sort_hobbies = hobby_all.sort_by { |hobby|[ -hobby.percentage, hobby.id ] }
+
+    # １週間分の曜日スロットを作成（曜日の残り枠として管理する）
+    days_slot = freetime.all.each_with_object({}) do |ft, hash|
+      # データ構造：{ FreeTimeオブジェクト => 120min, FreeTimeオブジェクト => 90, ... }
+      hash[ft] = ft.minutes
+    end
+    
+    # 各趣味の目標時間スロットを用意（目標時間の残り枠として管理する）
+    target_times_slot = @taget_time.dup
+
+    # 曜日と趣味用の添字を用意する
+    day_index = 0
+    hobby_index = 0
+
+    # スケジュールスロットの空き箱を作成（最終的な返り値とする）
+    hobby_schedule = hobby_all.each_with_object({}) do |hobby, hash|
+      # データ構造：{Hobbyオブジェクト => {}}この空いた{}にデータを詰めていく
+      hash[hobby] = {}
+    end
   end
 end
