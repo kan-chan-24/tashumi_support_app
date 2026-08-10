31606c920ccae1f84e88fc8ceb0f308d25aaa5ec
kan_chan <shunplay1793@gmail.com>
2026-08-10 21:26:32 +0900

    ルーティングの設定 結果発表ビューを作り、趣味入力>自由時間入力>結果発表の順で遷移するようルーティング 結果発表の中身については、ビューもコントローラーも未実装。 大枠と遷移のみ実装済み

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # 趣味入力フォーム（一覧 新規登録フォーム 削除）
   resources :hobbies, only: [ :index, :new, :create, :destroy ]
+  # 趣味入力フォームをルート設定
+  root "hobbies#index"
 
   # 自由時間入力フォーム
   resources :free_times, only: [ :index ] do
@@ -11,4 +13,7 @@ Rails.application.routes.draw do
       patch :update_all
     end
   end
+
+  # スケジュール計算結果を表示するビュー
+  resource :result, only: [ :show ]
 end
