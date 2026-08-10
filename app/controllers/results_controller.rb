710e43a6ec02f3e1341c66ccb5f5b6441bcdcf73
kan_chan <shunplay1793@gmail.com>
2026-08-10 22:43:41 +0900

    結果発表画面への遷移アクションの処理を実装 5パターンの提案パターンの計算結果を導き出し、一つのハッシュまとめる パターンには、キャッチコピーも一つずつ添えている

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,26 @@
 class ResultsController < ApplicationController
   def show
-  
+    # 目標時間を導き出す
+    target_times = HobbyTimeCalculator.call(hobbies: Hobby.all, free_times: FreeTime.all)
+
+    # パターンAを呼び出し保存
+    pattern_a = PatternAbCalculator.call(target_times: target_times, sort_direction: :desc)
+    # パターンB
+    pattern_b = PatternAbCalculator.call(target_times: target_times, sort_direction: :asc)
+    # パターンC
+    pattern_c = PatternCCalculator.call(target_times: target_times)
+    # パターンD
+    pattern_d = PatternDeCalculator.call(target_times: target_times, sort_direction: :desc)
+    # パターンE
+    pattern_e = PatternDeCalculator.call(target_times: target_times, sort_direction: :asc)
+
+    # 5パターンをまとめてハッシュにする
+    @patterns = {
+      "A" => { catchphrase: "...", schedule: pattern_a },
+      "B" => { catchphrase: "...", schedule: pattern_b },
+      "C" => { catchphrase: "...", schedule: pattern_c },
+      "D" => { catchphrase: "...", schedule: pattern_d },
+      "E" => { catchphrase: "...", schedule: pattern_e }
+    }
   end
 end
