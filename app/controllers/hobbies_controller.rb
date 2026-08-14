534491e12384778714a6437f4564d04e247d7b45
kan_chan <shunplay1793@gmail.com>
2026-08-14 16:05:30 +0900

    趣味の割合入力の非同期化 スライダー入力と入力フォームでの入力実装 非同期で趣味の追加 円グラフの数値非同期対応

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,18 +1,31 @@
 class HobbiesController < ApplicationController
   def index
     @hobbies = Hobby.order(:id)
-  end
-
-  def new
     @hobby = Hobby.new
   end
 
   def create
-    @hobby = Hobby.new(hobby_params)
-    if @hobby.save
-      redirect_to hobbies_path, notice: "趣味を登録しました"
+    last_hobby = Hobby.last
+
+    # 趣味の登録がすでにある場合/ない場合
+    if last_hobby
+      # あるなら、最後尾の趣味の半分の％で趣味を追加する
+      half_percentage = last_hobby.percentage / 2
+      last_hobby.update(percentage: last_hobby.percentage - half_percentage)
+      @hobby = Hobby.new(name: hobby_params[:name], percentage: half_percentage)
     else
-      render :new, status: :unprocessable_entity
+      # ないなら、100%で入れる
+      @hobby = Hobby.new(name: hobby_params[:name], percentage: 100)
+    end
+
+    if @hobby.save
+      @hobbies = Hobby.order(:id)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to hobbies_path, notice: "趣味を登録しました" }
+      end
+    else
+      redirect_to hobbies_path, alert: "趣味名を入力してください"
     end
   end
 
@@ -24,7 +37,7 @@ class HobbiesController < ApplicationController
   private
 
   def hobby_params
-    # 趣味の名前と割合（%）を渡す
-    params.require(:hobby).permit(:name, :percentage)
+    # 趣味の名前を渡す
+    params.require(:hobby).permit(:name)
   end
 end
